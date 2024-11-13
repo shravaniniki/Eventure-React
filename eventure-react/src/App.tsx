7907df5c19efa08c540dbd3372e5282562debfb3
varshini-sriram <varshinisriram@outlook.com>
2024-11-13 16:24:58 +0530

    ListEvents, EventDetails

--- eventure-react/src/App.tsx
+++ eventure-react/src/App.tsx
@@ -1,25 +1,31 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/shared/Header";
 import Home from "./pages/Home";
+import ListEvents from "./components/shared/events/ListEvents";
+import EventDetails from "./components/shared/events/EventDetails";
+import Footer from "./components/shared/Footer";
+
 
 
 function App() {
- 
-
   return (
-    
-   <BrowserRouter>
-   {/* <Header/> */}
-    <Routes>
-    <Route path="/" element={<Home />} />
-    
-    </Routes>
-    {/* <Footer/> */}
-
-   </BrowserRouter>
-   
-  )
+    <>
+      <BrowserRouter>
+        <Header />
+        <main className="container mt-5 pt-2">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/events/list" element={<ListEvents />} />
+            <Route path="/events/:id" element={<EventDetails />} /> 
+            
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+
+export default App;
--- eventure-react/src/models/IEvents.ts
+++ eventure-react/src/models/IEvents.ts
@@ -1,6 +1,13 @@
-export interface IEvent {
-  id?: number; // optional property
-  name: string;
-  phone: string;
-  email: string;
+export interface IEvent{
+  id?: number;
+  name: string;             // Name of the event
+  organizer: string;        // Organizer of the event
+  email: string;            // Organizer's email 
+  phone: string;            // Organizer's phone number
+  e_date: Date;             // Event date
+  duration: string;         // Duration of the event
+  location: string;         // Location of the event     
+  maxParticipants: number;   // Maximum participants allowed
+  e_mode: string;           // Mode of the event (e.g., online, offline)
+  description: string;      // Description of the event
 }
